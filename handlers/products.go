dbf57b8cd06fda0ac2b4589251ba139a55c503b1
Sevban Bayrak <sevban.bayrak@sabancidx.com>
2021-07-13 12:54:39 +0300

    handle http methods

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/sbvnbyrk/go-microservice/data"
 )
@@ -16,10 +18,38 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet{
-		p.getProducts(rw,r)
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
 		return
 	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+		q := regexp.MustCompile(`/([0-9]+)`)
+
+		g := q.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "invalid uri", http.StatusBadRequest)
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "invalid uri", http.StatusBadRequest)
+		}
+
+		isString := g[0][1]
+		id, err := strconv.Atoi(isString)
+		if err != nil {
+			http.Error(rw, "invalid uri", http.StatusBadRequest)
+		}
+		p.updateProducts(id, rw, r)
+	}
+
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -28,5 +58,42 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "unable json writer", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Request")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
 	}
 }
